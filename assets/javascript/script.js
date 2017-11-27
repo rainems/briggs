7f93a602acd78d1c5d33cd16be34d643119abeac
Raine Scott <rainems3@gmail.com>
2017-11-26 23:58:15 -0800

    attempted click

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,38 +1,55 @@
 
-/*
+
 $(document).ready(function () {
+    /*
 
-    $('html, body').css({
-        overflow: 'hidden',
-        height: '100%'
-    });
+    var visible = false;
+    var event_before = null;
+    var event_after = null;
 
 
-    var tenth = $(window).height() * .1;
-    //var wrap = $("#wrap");
-    var wrap = $("body");
+    $(window).click(function(e) {
+        if(visible){
+            event_after.style = event_before
+            event_after.style.height = "25vh";
 
-    var scroll_lengths = [7 * tenth, 9.5 * tenth, 12 * tenth, 14.5 * tenth, 17 * tenth]
-    var index = 0;
-    var first = true;
-    wrap.on("scroll", function (e) {
-        if (first) {
-            first = false;
-            $(".relative").css("animation", "relativeAnim 1.0s forwards");
-            $(".clear").css("animation", "clearAnim 1.0s forwards");
-            $(".splash").css("animation", "splashAnim 1.0s forwards")
-        }
+            $(".division-pic").each(function(){
+                $(this).css({"opacity":"1"})
+            });
 
-        if (index < scroll_lengths.length - 1 && this.scrollTop > scroll_lengths[index] - 3) {
-            index++;
-            $("#" + String(index - 1)).css({"position": "fixed", "top": "30vh", "left": String((index - 1) * 15 + 10) + "vw", "margin":"0" });
-            $("<div class='division'> <div class='division-pic'></div> </div>").insertBefore("#" + String(index -1) )
         }
     });
+
+    $(".division-pic").click(function(e){
+        event_before = e.target.style;
+        console.log(event_before)
+
+        $(".division-pic").each(function(){
+            $(this).css("opacity", "0.0")
+        });
+
+        $(e.target).css({"height":"50vh", "width":"75vw", "opacity":"1", "left":"12.5vw"})
+        event.stopPropagation();
+        visible = true;
+        event_after = e.target
+    })
+    */
+    /*
+
+
+
+    click disabled due to bug :(
+     */
+
+
+
+
+
+
 
 
 });
-*/
+
 
 var tenth = $(window).height() * .1;
 var scroll_lengths = [7 * tenth, 9.5 * tenth, 12 * tenth, 14.5 * tenth, 17 * tenth, 22 * tenth, 24.5 * tenth, 27 * tenth, 29.5 * tenth, 32 * tenth]
